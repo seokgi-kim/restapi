d7a9753fc5f1b2daa2791298913ed4ac9262d45a
seokgi <seokgi4885@gmail.com>
2020-08-15 22:34:25 +0900

    add event Controller test

--- src/test/java/com/sak/restapi/event/EventControllerTest.java
+++ src/test/java/com/sak/restapi/event/EventControllerTest.java
@@ -1,4 +1,54 @@
 package com.sak.restapi.event;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
 public class EventControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void createEvent() throws Exception {
+
+        Event event = Event.builder()
+                        .name("Spring")
+                        .description("REST API Development with Spring")
+                        .beginEnrollmentDateTime(LocalDateTime.of(2020, Month.AUGUST, 15, 22, 0))
+                        .closeEnrollmentDateTime(LocalDateTime.of(2020, Month.AUGUST, 16, 22, 0))
+                        .beginEventDateTime(LocalDateTime.of(2020, Month.AUGUST, 17, 22, 0))
+                        .endEventDateTime(LocalDateTime.of(2020, Month.AUGUST, 17, 23, 0))
+                        .basePrice(100)
+                        .maxPrice(200)
+                        .limitOfEnrollment(100)
+                        .location("선릉역 스타벅스")
+                        .build();
+
+        mockMvc.perform(post("/api/events/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
+    }
+
 }
