54a9283e3c251349990d442a8d03ad76faf45d50
seokgi <seokgi4885@gmail.com>
2020-08-16 18:17:37 +0900

    validate wrong data

---
+++ src/main/java/com/sak/restapi/event/EventValidator.java
@@ -0,0 +1,25 @@
+package com.sak.restapi.event;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Component
+public class EventValidator {
+
+    public void validate(EventDto eventDto, Errors errors) {
+        if(eventDto.getBasePrice() > eventDto.getMaxPrice()
+        && eventDto.getMaxPrice() > 0) {
+            errors.rejectValue("basePrice", "wrongValue", "BasePrice is wrong");
+        }
+
+        LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
+        if(endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
+        endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime()) ||
+        endEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())) {
+            errors.rejectValue("endEventDateTime", "wrongValue", "endEventDateTime is wrong");
+        }
+    }
+}
